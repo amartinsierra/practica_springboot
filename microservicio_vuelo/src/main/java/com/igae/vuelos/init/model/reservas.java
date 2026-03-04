0ea484933821f99d37f05e7d042da4b6f2aa34f7
artulance <artudevweb@gmail.com>
2026-03-04 11:00:20 +0100

    commit inicial

---
+++ microservicio_vuelo/src/main/java/com/igae/vuelos/init/model/reservas.java
@@ -0,0 +1,23 @@
+package com.igae.vuelos.init.model;
+
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="reservas")
+public class reservas {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String nombre;
+	private String dni;
+	private String vuelo;
+}
