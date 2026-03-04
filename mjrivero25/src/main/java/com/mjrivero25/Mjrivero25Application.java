1254298000ed0b63c9c40cf1d280cc7d425342d7
mjrivero25 <manuriveroa25@gmail.com>
2026-03-04 10:47:58 +0100

    preparacion practica

---
+++ mjrivero25/src/main/java/com/mjrivero25/Mjrivero25Application.java
@@ -0,0 +1,13 @@
+package com.mjrivero25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mjrivero25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mjrivero25Application.class, args);
+	}
+
+}
---
+++ mjrivero25/src/test/java/com/mjrivero25/Mjrivero25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjrivero25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mjrivero25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
